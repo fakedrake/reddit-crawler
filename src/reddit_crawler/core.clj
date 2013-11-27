1f772e9c1a427d4bb1d9f4b884e9bfa1bdd5d4f4
Chris Perivolaropoulos <darksaga2006@gmail.com>
2013-11-27 21:31:11 +0200

    Moved stuff to util and started retrievers

--- src/reddit_crawler/core.clj
+++ src/reddit_crawler/core.clj
@@ -1,44 +1,12 @@
 (ns reddit-crawler.core
   (:require [reddit.clj.core :as reddit])
-  (:use [clojure.pprint :only [pprint]]))
+  (:use [clojure.pprint :only [pprint]]
+        [reddit-crawler.util]))
 
-
-(defn flatten-keys*
-  "Search in a tree appending each key in keys to create a vector of
-  keys. Then present it all in map of key-lists->leaves"
-  [return-map keys tree]
-
-  (if tree
-    (cond
-     (vector? tree) (apply merge
-                           (map
-                            (fn [v index]
-                              (flatten-keys* return-map (conj keys index) v))
-                            tree (range (count tree))))
-
-     (map? tree) (apply merge
-                        (map
-                         (fn [[k v]]
-                           (flatten-keys* return-map (conj keys k) v))
-                         (seq tree)))
-
-     :else (assoc return-map keys tree))))
-
-(defn flatten-keys
-  "Find the path of keys for each leaf value."
-  [m] (flatten-keys* {} [] (vec m)))
-
-
-(defn get-items
-  "Get items from a tree based on their immediate parent."
-  [tree key]
-  (apply sorted-set
-         (remove nil?
-                 (map
-                  (fn [[k v]]
-                    (if (= (last k) key) v))
-                  (seq (flatten-keys tree))))))
-
+(defn login
+  "Make sure to return a valid rc"
+  [creds]
+  (or (:rc creds) (reddit/login (:user creds) (:pass creds))))
 
 (defn comment-authors
   "Get the authors of a reddit, a list of comments, a comment etc."
@@ -49,11 +17,6 @@
   "Get post ids from author's posts"
   [author-posts]
   (get-items author-posts :id))
-
-(defn login
-  "Make sure to return a valid rc"
-  [creds]
-  (or (:rc creds) (reddit/login (:user creds) (:pass creds))))
 
 (defn neighbour-post-authors
   "Get psot neighboring authors. Login or login anonymously"
@@ -70,28 +33,41 @@
 
 (def neighbour-author-posts-m (memoize neighbour-author-posts))
 
-(defn lazy-mapcat
-  [f coll]
-  (lazy-seq
-   (if (not-empty coll)
-     (concat
-      (f (first coll))
-      (lazy-mapcat f (rest coll))))))
-
 (defn neighbour-posts
   "Get neighboring posts. First we get all of the first author then
-  all the second etc."
+  all the second etc. This function should be able to use hooks."
   [post & {:keys [rc user pass order]
            :or {order 1}
            :as creds}]
+
   (let [rc (login creds)
         posts (if (coll? post) post [post])
         authors (lazy-mapcat #(neighbour-post-authors-m % :rc rc) posts)
         neighbours (lazy-mapcat #(neighbour-author-posts-m % :rc rc) authors)]
-    (println "order: " order ", size:" (count neighbours))
     (if (> order 1)
       (lazy-mapcat #(neighbour-posts % :order (dec order) :rc rc) neighbours)
       neighbours)))
+
+;; A list of predicates that will pull from a post a single lazy seq
+;; of other posts and rate them
+
+(defn comment-retriever
+  "Retriever using the comments authors."
+  [post rating & rc]
+  (let [authors (lazy-enumerate (neighbour-post-authors-m posts :rc rc))
+        posts (map (fn [[i a]] [(/ 1 i) (neighbour-author-posts-m a :rc rc)])
+                   authors)]
+    (lazy-mapcat
+     (fn [[ra authors-posts]]
+       (map (fn [[ip p]]
+              [(* ra (/ 1 ip)) p])
+            (lazy-enumerate authors-posts)))
+     posts)))
+
+(def post-retrievers
+  "Functions that receve a post and it's rating and optionally a login
+  cookie. return a lazy seq of conses of related (POST . RATING)."
+  nil)
 
 (defn unique-posts
   "From a list posts get a dict that show how many of each there was
@@ -101,8 +77,9 @@
 
 (defn post-prop
   "Get a post property given the id. Just so you konw :selftext is the
-  body and :title is the title" [post-id prop & {:keys [rc user pass
-  order]
+  body and :title is the title"
+  [post-id prop & {:keys [rc user pass
+                          order]
                    :or {order 1}
                    :as creds}]
   (first
---
+++ src/reddit_crawler/util.clj
@@ -0,0 +1,54 @@
+(ns reddit-crawler.util
+  (:use [clojure.pprint :only [pprint]]))
+
+(defn flatten-keys*
+  "Search in a tree appending each key in keys to create a vector of
+  keys. Then present it all in map of key-lists->leaves"
+  [return-map keys tree]
+
+  (if tree
+    (cond
+     (vector? tree) (apply merge
+                           (map
+                            (fn [v index]
+                              (flatten-keys* return-map (conj keys index) v))
+                            tree (range (count tree))))
+
+     (map? tree) (apply merge
+                        (map
+                         (fn [[k v]]
+                           (flatten-keys* return-map (conj keys k) v))
+                         (seq tree)))
+
+     :else (assoc return-map keys tree))))
+
+(defn flatten-keys
+  "Find the path of keys for each leaf value."
+  [m] (flatten-keys* {} [] (vec m)))
+
+(defn get-items
+  "Get items from a tree based on their immediate parent."
+  [tree key]
+  (apply sorted-set
+         (remove nil?
+                 (map
+                  (fn [[k v]]
+                    (if (= (last k) key) v))
+                  (seq (flatten-keys tree))))))
+
+(defn lazy-mapcat
+  "Like mapcat but lazy. Actually like python's chain of itertools."
+  [f coll]
+  (if (not-empty coll)
+    (concat
+     (f (first coll))
+     (lazy-seq (lazy-mapcat f (rest coll))))))
+
+(defn lazy-enumerate
+  "Return a lazy-seq tha starts at offset or 0."
+  ([coll] (lazy-enumerate coll 0))
+  ([coll offset]
+     (if (not-empty coll)
+       (cons
+        [offset (first coll)]
+        (lazy-seq (lazy-enumerate (rest coll) (inc offset)))))))
