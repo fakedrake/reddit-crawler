a9bbc74ca172e00a0d618de51a0f9ee8b2891b1e
Chris Perivolaropoulos <darksaga2006@gmail.com>
2013-12-03 21:52:19 +0200

    Comment retriever is a bit smarter now.

--- src/reddit_crawler/core.clj
+++ src/reddit_crawler/core.clj
@@ -48,14 +48,18 @@
       (lazy-mapcat #(neighbour-posts % :order (dec order) :rc rc) neighbours)
       neighbours)))
 
+;; XXX: This is basically an example. Retrievers should be implemented
+;; with packages that are also modular.
+;;
+;; For example here we have a perfectly capable comment-retriever but it can be much smarter when rating or
 (defn comment-retriever
-  "Retriever using the comments authors."
+  "Retriever using the post comments' authors."
   [post rating & rc]
-  (let [authors (lazy-enumerate (neighbour-post-authors-m posts :rc rc))
+  (let [authors (neighbour-post-authors-m posts :rc rc)
         grouped-posts (map (fn [[i a]]  ;XXX: use upvotes for rating
                              [(/ 1 i) (neighbour-author-posts-m a :rc rc)])
-                           authors)]
-    (lazy-mapcat                        ;Ungroup and rerate posts
+                           (lazy-enumerate authors))]
+    (lazy-mapcat                        ;Ungroup and re-rate posts
      (fn [[author-rating authors-posts]]
        (map (fn [[i p]]
               [(* author-rating (/ 1 ip)) p])
